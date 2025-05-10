85a583548777553782edfccb73159ee6f866ce63
Jang JunYeong <jjy0224y@kw.ac.kr>
2025-05-10 22:28:56 +0900

    add CustomizingControl and some resources

--- Project_SSAAC/CustomizingControl.Designer.cs
+++ Project_SSAAC/CustomizingControl.Designer.cs
@@ -29,11 +29,20 @@
         private void InitializeComponent()
         {
             this.btnBackToHome = new System.Windows.Forms.Button();
+            this.btnBodyPrev = new System.Windows.Forms.Button();
+            this.btnBodyNext = new System.Windows.Forms.Button();
+            this.lblBody = new System.Windows.Forms.Label();
+            this.picBoxCharacter = new System.Windows.Forms.PictureBox();
+            this.panel_menu = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxCharacter)).BeginInit();
+            this.panel_menu.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnBackToHome
             // 
-            this.btnBackToHome.Location = new System.Drawing.Point(3, 560);
+            this.btnBackToHome.Location = new System.Drawing.Point(3, 536);
             this.btnBackToHome.Name = "btnBackToHome";
             this.btnBackToHome.Size = new System.Drawing.Size(140, 37);
             this.btnBackToHome.TabIndex = 0;
@@ -41,13 +50,76 @@
             this.btnBackToHome.UseVisualStyleBackColor = true;
             this.btnBackToHome.Click += new System.EventHandler(this.btnBackToHome_Click);
             // 
+            // btnBodyPrev
+            // 
+            this.btnBodyPrev.BackColor = System.Drawing.Color.Transparent;
+            this.btnBodyPrev.Location = new System.Drawing.Point(16, 59);
+            this.btnBodyPrev.Name = "btnBodyPrev";
+            this.btnBodyPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnBodyPrev.TabIndex = 2;
+            this.btnBodyPrev.Text = "Prev";
+            this.btnBodyPrev.UseVisualStyleBackColor = false;
+            this.btnBodyPrev.Click += new System.EventHandler(this.btnBodyPrev_Click);
+            // 
+            // btnBodyNext
+            // 
+            this.btnBodyNext.Location = new System.Drawing.Point(216, 59);
+            this.btnBodyNext.Name = "btnBodyNext";
+            this.btnBodyNext.Size = new System.Drawing.Size(75, 23);
+            this.btnBodyNext.TabIndex = 1;
+            this.btnBodyNext.Text = "Next";
+            this.btnBodyNext.UseVisualStyleBackColor = true;
+            this.btnBodyNext.Click += new System.EventHandler(this.btnBodyNext_Click);
+            // 
+            // lblBody
+            // 
+            this.lblBody.AutoSize = true;
+            this.lblBody.Location = new System.Drawing.Point(151, 63);
+            this.lblBody.Name = "lblBody";
+            this.lblBody.Size = new System.Drawing.Size(15, 15);
+            this.lblBody.TabIndex = 0;
+            this.lblBody.Text = "1";
+            // 
+            // picBoxCharacter
+            // 
+            this.picBoxCharacter.Location = new System.Drawing.Point(115, 158);
+            this.picBoxCharacter.Name = "picBoxCharacter";
+            this.picBoxCharacter.Size = new System.Drawing.Size(50, 50);
+            this.picBoxCharacter.TabIndex = 4;
+            this.picBoxCharacter.TabStop = false;
+            // 
+            // panel_menu
+            // 
+            this.panel_menu.Controls.Add(this.panel1);
+            this.panel_menu.Controls.Add(this.picBoxCharacter);
+            this.panel_menu.Location = new System.Drawing.Point(147, 108);
+            this.panel_menu.Name = "panel_menu";
+            this.panel_menu.Size = new System.Drawing.Size(705, 360);
+            this.panel_menu.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnBodyPrev);
+            this.panel1.Controls.Add(this.btnBodyNext);
+            this.panel1.Controls.Add(this.lblBody);
+            this.panel1.Location = new System.Drawing.Point(302, 122);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 137);
+            this.panel1.TabIndex = 5;
+            // 
             // CustomizingControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel_menu);
             this.Controls.Add(this.btnBackToHome);
             this.Name = "CustomizingControl";
-            this.Size = new System.Drawing.Size(800, 600);
+            this.Size = new System.Drawing.Size(1024, 576);
+            this.Load += new System.EventHandler(this.CustomizingControl_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxCharacter)).EndInit();
+            this.panel_menu.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -55,5 +127,11 @@
         #endregion
 
         private System.Windows.Forms.Button btnBackToHome;
+        private System.Windows.Forms.Button btnBodyPrev;
+        private System.Windows.Forms.Button btnBodyNext;
+        private System.Windows.Forms.Label lblBody;
+        private System.Windows.Forms.PictureBox picBoxCharacter;
+        private System.Windows.Forms.Panel panel_menu;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- Project_SSAAC/CustomizingControl.cs
+++ Project_SSAAC/CustomizingControl.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Project_SSAAC.GameObjects;
+using Project_SSAAC.UI;
 
 namespace Project_SSAAC
 {
@@ -18,11 +20,68 @@ namespace Project_SSAAC
         {
             InitializeComponent();
             _main = main;
+
+            this.BackgroundImage = Properties.Resources.background_customizing;
+            picBoxCharacter.BackColor = Color.Transparent;
+            picBoxCharacter.Size = new Size(50, 50);
+            picBoxCharacter.SizeMode = PictureBoxSizeMode.StretchImage;
+            panel_menu.BackgroundImage = Properties.Resources.panel_menu;
+            panel_menu.BackColor = Color.Transparent;
+            
+            panel_menu.Size = new Size(600, 350);
+            UpdateCharacterImage();
         }
 
         private void btnBackToHome_Click(object sender, EventArgs e)
         {
             _main.LoadControl(new HomeControl(_main));
+        }
+
+        public Player customizedPlayer { get; private set; } = new Player();
+        private List<string> appearanceOptions = new List<string> { "blue", "green", "mask", "pink" };
+        private int currentAppearanceIndex = 0;
+        private void UpdateCharacterImage()
+        {
+            string appearance = appearanceOptions[currentAppearanceIndex];
+            
+            customizedPlayer.SelectedAppearance = appearance;
+
+
+            switch (appearance)
+            {
+                case "blue":
+                    picBoxCharacter.Image = Properties.Resources.playerBlue;
+                    break;
+                case "green":
+                    picBoxCharacter.Image = Properties.Resources.playerGreen;
+                    break;
+                case "mask":
+                    picBoxCharacter.Image = Properties.Resources.playerMasked;
+                    break;
+                case "pink":
+                    picBoxCharacter.Image = Properties.Resources.playerPink;
+                    break;
+            }
+            
+        }
+
+        private void btnBodyNext_Click(object sender, EventArgs e)
+        {
+            currentAppearanceIndex = (currentAppearanceIndex + 1) % appearanceOptions.Count;
+            lblBody.Text = (currentAppearanceIndex + 1).ToString();
+            UpdateCharacterImage();
+        }
+
+        private void btnBodyPrev_Click(object sender, EventArgs e)
+        {
+            currentAppearanceIndex = (currentAppearanceIndex -1) % appearanceOptions.Count;
+            lblBody.Text = (currentAppearanceIndex + 1).ToString();
+            UpdateCharacterImage();
+        }
+
+        private void CustomizingControl_Load(object sender, EventArgs e)
+        {
+            panel_menu.Location = new Point(_main.Width / 2 - panel_menu.Width / 2, this.Height / 2 - panel_menu.Height/2);
         }
     }
 }
